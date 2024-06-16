94922accf01db2828a24de702578b1327eb3908a
aeduz24 <aeduz2024@gmail.com>
2024-06-16 13:25:09 +0530

    added nit and iit jossa data

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def filter_data(df, gender, seat_type, quota, rank):
     ]
     return filtered_df
 
-st.title('AeduZ Rank Finder')
+st.title('AeduZ College Finder (Yearwise)')
 
 # User inputs
 # year = st.number_input('Enter Year:', min_value=2000, step=1)
@@ -29,22 +29,27 @@ st.title('AeduZ Rank Finder')
 st.sidebar.title('Filters')
 
 # User inputs in the sidebar
-year = st.sidebar.selectbox('Select Year', [2023])
+year = st.sidebar.selectbox('Select Year', [2020,2021,2022,2023])
+college=st.sidebar.selectbox('Select college',["IIT","NIT"])
 round_num = st.sidebar.selectbox('Select Round:', [1, 2, 3, 4, 6])
 gender = st.sidebar.selectbox('Select Gender:', ['Gender-Neutral', 'Female-only (including Supernumerary)'])
 seat_type = st.sidebar.selectbox('Select Seat Type:', ['OPEN', 'EWS', 'OBC-NCL', 'SC', 'ST', 'OPEN (PwD)', 'OBC-NCL (PwD)', 'EWS (PwD)', 'ST (PwD)', 'SC (PwD)'])
-quota = st.sidebar.selectbox('Select Quota:', ['AI', 'HS', 'OS', 'GO', 'JK', 'LA'])
+if college=="IIT":
+    quota = st.sidebar.selectbox('Select Quota:', ['AI', 'HS', 'OS', 'GO', 'JK', 'LA'])
+if college=="NIT":
+    quota = st.sidebar.selectbox('Select Quota:', ['HS', 'OS', 'GO', 'JK', 'LA'])
 rank = st.sidebar.number_input('Enter Rank:', min_value=0, step=1)
 
 
 # Load CSV based on user inputs
-filename = f'{year}-round{round_num}.csv'
+filename = f'{year}_round{round_num}_{college.lower()}.csv'
 try:
     df = pd.read_csv(filename)
 except FileNotFoundError:
     st.error(f"Error: File '{filename}' not found.")
     st.stop()
-
+df['Closing Rank']=df['Closing Rank'].astype(str)
+df['Opening Rank']=df['Opening Rank'].astype(str)
 df=df[~df['Closing Rank'].str.contains('P')]
 df=df[~df['Opening Rank'].str.contains('P')]
 df['Opening Rank']=df['Opening Rank'].astype(float)
